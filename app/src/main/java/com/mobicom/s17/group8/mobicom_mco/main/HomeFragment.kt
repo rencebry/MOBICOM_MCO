ae796af7fda3074369e384f9d1d49f8eec9f7044
kiwiwidom <119975948+kiwiwidom@users.noreply.github.com>
2025-06-28 21:39:27 +0800

    added card profile for home screen

--- app/src/main/java/com/mobicom/s17/group8/mobicom_mco/main/HomeFragment.kt
+++ app/src/main/java/com/mobicom/s17/group8/mobicom_mco/main/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.mobicom.s17.group8.mobicom_mco.main
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.mobicom.s17.group8.mobicom_mco.R
+import com.mobicom.s17.group8.mobicom_mco.auth.LandingActivity
 import com.mobicom.s17.group8.mobicom_mco.databinding.FragmentHomeBinding
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -29,6 +33,22 @@ class HomeFragment : Fragment() {
         val userName = "Kyoka" // placehlder
         binding.tvGreeting.text = getString(R.string.hello_user, userName) //placehlder
         setDate()
+
+        binding.ivSettings.setOnClickListener {
+            showLogoutDialog()
+        }
+
+        binding.btnStartStudying.setOnClickListener {
+            findNavController().navigate(R.id.nav_study)
+        }
+
+        binding.profileCard.apply {
+            tvNameValue.text = "Kyoka"
+            tvSchoolValue.text = "DLSU-M"
+            tvCourseValue.text = "BS CS-ST"
+            tvBirthdayValue.text = "06-04-2003"
+            tvYearLevelValue.text = "III"
+        }
     }
 
     private fun setDate() {
@@ -53,6 +73,29 @@ class HomeFragment : Fragment() {
             3 -> "rd"
             else -> "th"
         }
+    }
+
+    private fun showLogoutDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Logout")
+            .setMessage("Are you sure you want to log out?")
+            .setPositiveButton("Logout") { dialog, which ->
+                logoutUser()
+            }
+            .setNegativeButton("Cancel") { dialog, which ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun logoutUser() {
+        // Intent to go back to the LandingActivity
+        val intent = Intent(requireActivity(), LandingActivity::class.java).apply {
+            // clear the entire task stack
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        startActivity(intent)
+        requireActivity().finish()
     }
 
     override fun onDestroyView() {
