5b5812325f5a543cb331a8380b8cd0b02a1b9f97
rencebry <clarence_borlaza@dlsu.edu.ph>
2025-06-28 23:39:33 +0800

    Added New Task Screen

--- app/src/main/java/com/mobicom/s17/group8/mobicom_mco/todo/TodoFragment.kt
+++ app/src/main/java/com/mobicom/s17/group8/mobicom_mco/todo/TodoFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mobicom.s17.group8.mobicom_mco.R
 import com.mobicom.s17.group8.mobicom_mco.databinding.FragmentTodoListBinding
@@ -33,8 +34,7 @@ class TodoFragment : Fragment() {
 
         binding.addTaskFab.setOnClickListener {
             // Handle add task button click
-            // This could open a dialog or a new fragment to add a new task
-            // For now, we can just show a message or log it
+            findNavController().navigate(R.id.action_nav_todo_to_new_task)
         }
     }
 
