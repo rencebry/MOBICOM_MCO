36a6737b384c0b10ca11f5e5315161861fde4ef0
Elyana Omang <elyana_omang@dlsu.edu.ph>
2025-08-03 23:05:42 +0800

    Update Deck.kt

--- app/src/main/java/com/mobicom/s17/group8/mobicom_mco/study/Deck.kt
+++ app/src/main/java/com/mobicom/s17/group8/mobicom_mco/study/Deck.kt
@@ -1,9 +1,16 @@
 package com.mobicom.s17.group8.mobicom_mco.study
 
+
+import java.util.UUID
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "deck")
 data class Deck(
+    @PrimaryKey val deckId: String = UUID.randomUUID().toString(),
     val courseId: String,
-    val deckId: String,
-    val name: String,
+    val deckTitle: String,
+    val dateCreated: String,
     val cardCount: Int,
-    val dateCreated: String
+    var isFavorite: Boolean = false
 )
